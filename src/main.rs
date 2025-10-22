9161a822ed529fafc7026167ea173ed82f18bd45
Gabriel Pozo <jackgris2@gmail.com>
2025-10-21 21:46:36 -0300

    refactor: using struct instead of tuple

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,27 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for '{}' in file '{}'", query, file_path);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Something went wrong reading the file");
+    println!(
+        "Searching for '{}' in file '{}'",
+        config.query, config.file_path
+    );
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
